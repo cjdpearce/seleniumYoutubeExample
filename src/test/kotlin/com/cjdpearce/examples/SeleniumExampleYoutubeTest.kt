2e0e0227082563def078d320aae696daeee23252
Craig Pearce <craig.pearce@skybettingandgaming.com>
2019-12-05 15:51:41 +0000

    refactored pom an removed unneeded libs an dirs from directory

--- src/test/kotlin/com/cjdpearce/examples/SeleniumExampleYoutubeTest.kt
+++ src/test/kotlin/com/cjdpearce/examples/SeleniumExampleYoutubeTest.kt
@@ -3,24 +3,24 @@ package com.cjdpearce.examples
 import org.junit.Test
 
 class SeleniumExampleYoutubeTest {
-    private val ser: SeleniumExampleYoutube = SeleniumExampleYoutube()
+    private val sey: SeleniumExampleYoutube = SeleniumExampleYoutube()
 
     @Test
     fun search(){
-        ser.youtubeSearch("Sky Betting and Gaming")
-        ser.dr.closeDriver()
+        sey.youtubeSearch("Sky Betting and Gaming")
+        sey.dr.closeDriver()
     }
 
     @Test
     fun searchAndFindChannel(){
-        ser.gotoChannel("Sky Betting & Gaming")
-        ser.dr.closeDriver()
+        sey.gotoChannel("Sky Betting & Gaming")
+        sey.dr.closeDriver()
     }
 
     @Test
     fun findChannelAndOpenVideo(){
-        ser.gotoChannel("Sky Betting & Gaming")
-        ser.findTabContent("videos")
-        ser.clickVideo("sky betting & gaming tech academy")
+        sey.gotoChannel("Sky Betting & Gaming")
+        sey.findTabContent("videos")
+        sey.clickVideo("sky betting & gaming tech academy")
     }
 }
